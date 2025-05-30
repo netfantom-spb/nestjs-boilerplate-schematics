b056f7d50ef1f588a0bf4a2ea251615a93b10fd0
Viktor Egorov <netfantom.spb@gmail.com>
2025-05-30 13:19:59 +0300

    feat: add logger to service constructor

--- src/lib/service/files/ts/__name__.service.ts
+++ src/lib/service/files/ts/__name__.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { InjectPinoLogger, PinoLogger } from 'nestjs-pino';
 
 @Injectable()
-export class <%= classify(name) %>Service {}
+export class <%= classify(name) %>Service {
+    constructor(
+        @InjectPinoLogger(<%= classify(name) %>Service.name)
+        private readonly logger: PinoLogger) {}
+}
